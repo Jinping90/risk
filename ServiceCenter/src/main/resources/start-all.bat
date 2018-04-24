4b8bf3e89bbcfbc4f47872f3a6c39b5fb5f8f6bb
zhaojinping <zhaojinping@iqurong.com>
2018-04-24 16:07:34 +0800

    Revert "增加7个微服务"
    This reverts commit a0dca48

--- ServiceCenter/src/main/resources/start-all.bat
+++
@@ -1,3 +0,0 @@
-start java -jar ServiceCenter-0.0.1.jar --spring.profiles.active=peer1
-start java -jar ServiceCenter-0.0.1.jar --spring.profiles.active=peer2
-start java -jar ServiceCenter-0.0.1.jar --spring.profiles.active=peer3
--- ServiceCenter/src/main/resources/start2.bat
+++
@@ -1,1 +0,0 @@
-start java -jar ServiceCenter-0.0.1.jar --spring.profiles.active=peer2
